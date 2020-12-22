bd54ec2152552970a222009427c25b613ba62aa4
cool-ash <42828760+cool-ash@users.noreply.github.com>
2020-12-22 11:28:39 +0200

    Add files via upload

---
+++ web/app.py
@@ -0,0 +1,156 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api,Resource
+from pymongo import MongoClient
+
+app=Flask(__name__)
+api=Api(app)
+
+client=MongoClient("mongodb://db:27017")     #27017 is the default port on which mongodb works on.
+db=client.aNewDB
+UserNum=db['UserNum']       #UserNum is a new collection here.
+UserNum.insert({
+    'num_of_users':0
+})
+
+
+class Visit(Resource):
+    def get(self):
+        prev_num=UserNum.find({})[0]['num_of_users']
+        new_num=prev_num+1
+        UserNum.update({},{"$set":{"num_of_users":new_num}})
+        return str("Hello user"+str(new_num))
+
+
+def checkPostedData(postedData,functionName):
+    if (functionName=="add" or functionName=="subtract" or functionName=="multiply"):
+        if "x" not in postedData or "y" not in postedData:
+            return 301
+        else:
+            return 200
+    elif(functionName=="divison"):
+        if "x" not in postedData or "y" not in postedData:
+            return 301
+        elif int(postedData["y"])==0:
+            return 302
+        else:
+            return 200
+
+
+class Add(Resource):                              
+    def post(self):
+        #If I am here than resource Add was requested using method POST
+        #Step 1: Get posted data:
+        postedData=request.get_json(force=True)
+        #Step2: Verify Validity of posted data
+        status_code=checkPostedData(postedData,"add")
+        if (status_code!=200):
+            retJson={
+                "Message":"An error happened",
+                "Status Code":status_code
+            }
+            return jsonify(retJson)
+        #If i am here, the status_code=200
+        x=postedData["x"]
+        y=postedData["y"]
+        x=int(x)
+        y=int(y)
+        #Add the posted data
+        ret=x+y
+        retMap={
+            'Message':ret,
+            'Status Code':200
+        }
+        return jsonify(retMap)                                          
+
+class Subtract(Resource):
+     def post(self):
+        #If I am here than resource Add was requested using method POST
+        #Step 1: Get posted data:
+        postedData=request.get_json(force=True)
+        #Step2: Verify Validity of posted data
+        status_code=checkPostedData(postedData,"subtract")
+        if (status_code!=200):
+            retJson={
+                "Message":"An error happened",
+                "Status Code":status_code
+            }
+            return jsonify(retJson)
+        #If i am here, the status_code=200
+        x=postedData["x"]
+        y=postedData["y"]
+        x=int(x)
+        y=int(y)
+        #Add the posted data
+        ret=x-y
+        retMap={
+            'Message':ret,
+            'Status Code':200
+        }
+        return jsonify(retMap)  
+
+
+class Multiply(Resource):
+    def post(self):
+        #If I am here than resource Add was requested using method POST
+        #Step 1: Get posted data:
+        postedData=request.get_json(force=True)
+        #Step2: Verify Validity of posted data
+        status_code=checkPostedData(postedData,"multiply")
+        if (status_code!=200):
+            retJson={
+                "Message":"An error happened",
+                "Status Code":status_code
+            }
+            return jsonify(retJson)
+        #If i am here, the status_code=200
+        x=postedData["x"]
+        y=postedData["y"]
+        x=int(x)
+        y=int(y)
+        #Add the posted data
+        ret=x*y
+        retMap={
+            'Message':ret,
+            'Status Code':200
+        }
+        return jsonify(retMap)  
+
+class Divide(Resource):
+    def post(self):
+        #If I am here than resource Add was requested using method POST
+        #Step 1: Get posted data:
+        postedData=request.get_json(force=True)
+        #Step2: Verify Validity of posted data
+        status_code=checkPostedData(postedData,"divison")
+        if (status_code!=200):
+            retJson={
+                "Message":"An error happened",
+                "Status Code":status_code
+            }
+            return jsonify(retJson)
+        #If i am here, the status_code=200
+        x=postedData["x"]
+        y=postedData["y"]
+        x=int(x)
+        y=int(y)
+        #Add the posted data
+        ret=(x*1.0)/y
+        retMap={
+            'Message':ret,
+            'Status Code':200
+        }
+        return jsonify(retMap)  
+
+api.add_resource(Add,"/add")
+api.add_resource(Subtract,"/subtract")
+api.add_resource(Multiply,"/multiply")
+api.add_resource(Divide,"/divide")
+api.add_resource(Visit,"/hello")
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
